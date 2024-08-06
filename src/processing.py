6ef24031a7a2ae205f68f0f9e9715c9ef1dc3c2a
Yana Sozonova <ynakochneva@mail.ru>
2024-08-06 15:53:28 +0500

    add filter_by_state in processing.py

--- src/processing.py
+++ src/processing.py
@@ -1,4 +1,4 @@
-def filter_by_state(list_dict: list, state='EXECUTED') ->list:
+def filter_by_state(list_dict: list, state: str = "EXECUTED") -> list:
     """Функция возвращает новый список словарей, у которых ключ
     state соответствует указанному значению"""
     new_list = [item for item in list_dict if item["state"] == state]
@@ -6,10 +6,14 @@ def filter_by_state(list_dict: list, state='EXECUTED') ->list:
     return new_list
 
 
-print(filter_by_state([
-    {'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
-    {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
-    {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
-    {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}],
-'CANCELED'
-))
+print(
+    filter_by_state(
+        [
+            {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
+            {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
+            {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
+            {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
+        ],
+        "CANCELED",
+    )
+)
