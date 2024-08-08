6df0d2ec7c42163e1fc0b95fcecd42397f3e3afa
Yana Sozonova <ynakochneva@mail.ru>
2024-08-08 21:48:44 +0500

    добавлены тесты для функции get_mask_account и обновлена сама функция

--- src/masks.py
+++ src/masks.py
@@ -6,18 +6,22 @@ def get_mask_card_number(card_number: Union[int, str]) -> str:
 
     mask_card_str = str(card_number)
 
-    if mask_card_str.isalpha() or len(mask_card_str) < 16 or len(mask_card_str) > 16:
-        return "Некорректный номер карты"
-
     if mask_card_str == "0" or mask_card_str == "":
         return "0"
+
+    if mask_card_str.isalpha() or len(mask_card_str) < 16 or len(mask_card_str) > 16:
+        return "Некорректный номер карты"
 
     return mask_card_str[:4] + " " + mask_card_str[4:6] + "** " + "*" * 4 + " " + mask_card_str[12:]
 
 
 def get_mask_account(account_number: Union[int, str]) -> str:
     """Функция принимает на вход номер счета и возвращает его маску"""
+    account_number_str = str(account_number)
 
-    account_mask = "**" + str(account_number)[-4:]
+    if account_number_str.isalpha() or len(account_number_str) < 20 or len(account_number_str) > 20:
+        return "Некорректный номер счета"
+
+    account_mask = "**" + account_number_str[-4:]
 
     return account_mask
--- tests/test_masks.py
+++ tests/test_masks.py
@@ -22,7 +22,22 @@ def test_get_mask_card_number_zero_number(card_number, expected):
 def expected():
     return "Некорректный номер карты"
 @pytest.mark.parametrize("card_number", [(70007922896),
-                                        ("70007922896063612233"),
+                                        ("700079228960636122333489789745"),
                                         ("1544вполло")])
 def test_get_mask_card_number_length(card_number, expected):
     assert get_mask_card_number(card_number) == expected
+
+def test_get_mask_account():
+    assert get_mask_account(73654108430135874305) == "**4305"
+
+
+@pytest.fixture()
+def expected_account():
+    return "Некорректный номер счета"
+@pytest.mark.parametrize("account", [(70007922896),
+                                    ("73654108430135874305265454"),
+                                    ("8574874вполло"),
+                                    (0),
+                                    (""), ])
+def test_get_mask_account_length(account, expected_account):
+    assert get_mask_account(account) == expected_account
