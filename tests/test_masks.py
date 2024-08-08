4306fd61979c8bdb2e2bf1f2a850190cadf82cce
Yana Sozonova <ynakochneva@mail.ru>
2024-08-08 17:39:08 +0500

    добавлен тест test_get_mask_card_number на правильности маскирования номера карты

---
+++ tests/test_masks.py
@@ -0,0 +1,11 @@
+import pytest
+
+from src.masks import get_mask_account, get_mask_card_number
+
+
+@pytest.mark.parametrize("card_number, expected", [(7000792289606361, "7000 79** **** 6361"),
+                                                   ("7000792289606361", "7000 79** **** 6361"),
+                                                   ])
+def test_get_mask_card_number(card_number, expected):
+    assert get_mask_card_number(card_number) == expected
+
