c3068434504f3fbba909f2b30201239d4f6d34ff
Yana Sozonova <ynakochneva@mail.ru>
2024-08-08 17:58:58 +0500

    добавлен тест test_get_mask_card_number_zero_number и обновлена функция get_mask_card_number

--- src/masks.py
+++ src/masks.py
@@ -4,6 +4,9 @@ from typing import Union
 def get_mask_card_number(card_number: Union[int, str]) -> str:
     """Функция принимает на вход номер карты и возвращает ее маску"""
 
+    if str(card_number) == "0" or str(card_number) == "":
+        return "0"
+
     mask_card_str = str(card_number)
 
     return mask_card_str[:4] + " " + mask_card_str[4:6] + "** " + "*" * 4 + " " + mask_card_str[12:]
--- tests/test_masks.py
+++ tests/test_masks.py
@@ -9,3 +9,10 @@ from src.masks import get_mask_account, get_mask_card_number
 def test_get_mask_card_number(card_number, expected):
     assert get_mask_card_number(card_number) == expected
 
+
+@pytest.mark.parametrize("card_number, expected", [(0, '0'),
+                                                   ("0", '0'),
+                                                   ("", '0')
+                                                   ])
+def test_get_mask_card_number_zero_number(card_number, expected):
+    assert get_mask_card_number(card_number) == expected
