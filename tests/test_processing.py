c45f713e7513186cfcf35e98ae24fd66a8619519
Yana Sozonova <ynakochneva@mail.ru>
2024-08-09 18:04:05 +0500

    test_processing.py, добавлены тесты  для функции filter_by_state и внесены мзменения в нее

---
+++ tests/test_processing.py
@@ -0,0 +1,59 @@
+import pytest
+
+from src.processing import filter_by_state
+
+
+@pytest.fixture()
+def my_list():
+    return [
+        {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
+        {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
+        {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
+        {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
+    ]
+
+
+def test_filter_by_state(my_list):
+    assert filter_by_state(my_list) == [
+        {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
+        {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
+    ]
+
+    assert filter_by_state(my_list, "CANCELED") == [
+        {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
+        {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
+    ]
+
+
+def test_filter_by_state_zero_state(my_list):
+    assert filter_by_state(my_list, "nothing") == []
+
+
+@pytest.mark.parametrize(
+    "state, expected",
+    [
+        (
+            "canceled",
+            [
+                {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
+                {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
+            ],
+        ),
+        (
+            "Executed",
+            [
+                {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
+                {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
+            ],
+        ),
+        (
+            "cancEled",
+            [
+                {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
+                {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
+            ],
+        ),
+    ],
+)
+def test_filter_by_state_different_meanings_state(my_list, state, expected):
+    assert filter_by_state(my_list, state) == expected
--- tests/test_wedget.py
+++ tests/test_wedget.py
@@ -1,6 +1,6 @@
 import pytest
 
-from src.wedget import mask_account_card, get_date
+from src.wedget import get_date, mask_account_card
 
 
 def test_mask_account_card():
@@ -8,22 +8,29 @@ def test_mask_account_card():
     assert mask_account_card("Счет 64686473678894779589") == "Счет **9589"
 
 
-@pytest.mark.parametrize("account_card, expected",
-                         [("Visa Classic 70007922еннр", "Некорректный номер карты"),
-                          ("Мир 700079225748675423412", "Некорректный номер карты"),
-                          ("счет 64686473678896564231214559", "Некорректный номер счета"),
-                          ("Счет 0", "Некорректный номер счета"),
-                          ("", "0"),
-                          ])
+@pytest.mark.parametrize(
+    "account_card, expected",
+    [
+        ("Visa Classic 70007922еннр", "Некорректный номер карты"),
+        ("Мир 700079225748675423412", "Некорректный номер карты"),
+        ("счет 64686473678896564231214559", "Некорректный номер счета"),
+        ("Счет 0", "Некорректный номер счета"),
+        ("", "0"),
+    ],
+)
 def test_mask_account_card_invalid_input(account_card, expected):
     assert mask_account_card(account_card) == expected
 
 
-@pytest.mark.parametrize("account_card, expected",
-                         [("Maestro 7000792289606361", "Maestro 7000 79** **** 6361"),
-                          ("Мир 7000792289606361", "Мир 7000 79** **** 6361"),
-                          ("корреспондентский счет 73654108430135874305", "корреспондентский счет **4305"),
-                          ("Расчетный счет 73654108430135874305", "Расчетный счет **4305"), ])
+@pytest.mark.parametrize(
+    "account_card, expected",
+    [
+        ("Maestro 7000792289606361", "Maestro 7000 79** **** 6361"),
+        ("Мир 7000792289606361", "Мир 7000 79** **** 6361"),
+        ("корреспондентский счет 73654108430135874305", "корреспондентский счет **4305"),
+        ("Расчетный счет 73654108430135874305", "Расчетный счет **4305"),
+    ],
+)
 def test_mask_account_card_types_of_account(account_card, expected):
     assert mask_account_card(account_card) == expected
 
@@ -37,25 +44,31 @@ def test_get_date():
     assert get_date("2024-03-11T02:26:18.671407") == "11.03.2024"
 
 
-@pytest.mark.parametrize("date, expected",
-                         [("", ""),
-                          ("0", "0"),
-                          (" ", " "),
-                          ])
+@pytest.mark.parametrize(
+    "date, expected",
+    [
+        ("", ""),
+        ("0", "0"),
+        (" ", " "),
+    ],
+)
 def test_get_date_not_date(date, expected):
     assert get_date(date) == expected
-
 
 
 @pytest.fixture()
 def expected_data():
     return "11.03.2024"
 
-@pytest.mark.parametrize("date",
-                         [("2024-03-11T02:26:18.671407"),
-                          ("2024-03-11"),
-                          ("2024-03-11T02:26:18"),
-                          ("2024-03-11 02:26:18.671407"),
-                          ])
+
+@pytest.mark.parametrize(
+    "date",
+    [
+        ("2024-03-11T02:26:18.671407"),
+        ("2024-03-11"),
+        ("2024-03-11T02:26:18"),
+        ("2024-03-11 02:26:18.671407"),
+    ],
+)
 def test_get_date_not_date(date, expected_data):
     assert get_date(date) == expected_data
