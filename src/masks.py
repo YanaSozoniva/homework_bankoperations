1b6bc772c93b9b9c700bc2a291aa4cd1d9207704
Yana Sozonova <ynakochneva@mail.ru>
2024-08-16 00:10:25 +0500

    update all file

--- src/masks.py
+++ src/masks.py
@@ -5,6 +5,11 @@ def get_mask_card_number(card_number: Union[int, str]) -> str:
     """Функция принимает на вход номер карты и возвращает ее маску"""
 
     mask_card_str = str(card_number)
+    if mask_card_str == "0" or mask_card_str == "":
+        return "0"
+
+    if mask_card_str.isalpha() or len(mask_card_str) < 16 or len(mask_card_str) > 16:
+        return "Некорректный номер карты"
 
     return mask_card_str[:4] + " " + mask_card_str[4:6] + "** " + "*" * 4 + " " + mask_card_str[12:]
 
@@ -12,6 +17,11 @@ def get_mask_card_number(card_number: Union[int, str]) -> str:
 def get_mask_account(account_number: Union[int, str]) -> str:
     """Функция принимает на вход номер счета и возвращает его маску"""
 
-    account_mask = "**" + str(account_number)[-4:]
+    account_number_str = str(account_number)
+
+    if account_number_str.isalpha() or len(account_number_str) < 20 or len(account_number_str) > 20:
+        return "Некорректный номер счета"
+
+    account_mask = "**" + account_number_str[-4:]
 
     return account_mask
